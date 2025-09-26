76aae6093b84f7dbb06bcfdb1030fa6d4a9f50f1
Kovalenko-SV <Kovalenko.Sta.V@nmu.one>
2025-09-26 17:21:06 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿GreetUser("Станіслав");
+
+void GreetUser(string UserName)
+{
+    Console.WriteLine($"Привіт,{UserName} !");
+    Console.WriteLine("Це виконання лабораторної роботи на тему CI/CD у TeamCity ");
+}
